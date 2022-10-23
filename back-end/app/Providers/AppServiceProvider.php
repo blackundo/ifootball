c1545772ea914db6c159be11ef3ad31fee87cf6a
blackundo <blackundovn@gmail.com>
2022-10-23 13:08:04 +0700

    update AppServiceProvider.php

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -2,9 +2,34 @@
 
 namespace App\Providers;
 
-use App\Service\Product\ProductServiceInterface;
+use App\Repositories\Blog\BlogRepository;
+use App\Repositories\Blog\BlogRepositoryInterface;
+use App\Repositories\Brand\BrandRepository;
+use App\Repositories\Brand\BrandRepositoryInterface;
+use App\Repositories\Category\CategoryRepository;
+use App\Repositories\Category\CategoryRepositoryInterface;
+use App\Repositories\Order\OrderRepository;
+use App\Repositories\Order\OrderRepositoryInterface;
+use App\Repositories\OrderDetail\OrderDetailRepository;
+use App\Repositories\OrderDetail\OrderDetailRepositoryInterface;
 use App\Repositories\Product\ProductRepository;
 use App\Repositories\Product\ProductRepositoryInterface;
+use App\Repositories\ProductComment\ProductCommentRepository;
+use App\Repositories\ProductComment\ProductCommentRepositoryInterface;
+use App\Service\Blog\BlogService;
+use App\Service\Blog\BlogServiceInterface;
+use App\Service\Brand\BrandService;
+use App\Service\Brand\BrandServiceInterface;
+use App\Service\Category\CategoryService;
+use App\Service\Category\CategoryServiceInterface;
+use App\Service\Order\OrderService;
+use App\Service\Order\OrderServiceInterface;
+use App\Service\OrderDetail\OrderDetailService;
+use App\Service\OrderDetail\OrderDetailServiceInterface;
+use App\Service\Product\ProductService;
+use App\Service\Product\ProductServiceInterface;
+use App\Service\ProductComment\ProductCommentService;
+use App\Service\ProductComment\ProductCommentServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,7 +41,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //product
+        //Product
         $this->app->singleton(
             ProductRepositoryInterface::class,
             ProductRepository::class
@@ -24,6 +49,60 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(
             ProductServiceInterface::class,
             ProductService::class
+        );
+        //Product Comment
+        $this->app->singleton(
+            ProductCommentRepositoryInterface::class,
+            ProductCommentRepository::class
+        );
+        $this->app->singleton(
+            ProductCommentServiceInterface::class,
+            ProductCommentService::class
+        );
+        //Blog
+        $this->app->singleton(
+            BlogRepositoryInterface::class,
+            BlogRepository::class
+        );
+        $this->app->singleton(
+            BlogServiceInterface::class,
+            BlogService::class
+        );
+        //Category
+        $this->app->singleton(
+            CategoryRepositoryInterface::class,
+            CategoryRepository::class
+        );
+        $this->app->singleton(
+            CategoryServiceInterface::class,
+            CategoryService::class
+        );
+        //Brand
+        $this->app->singleton(
+            BrandRepositoryInterface::class,
+            BrandRepository::class
+        );
+        $this->app->singleton(
+            BrandServiceInterface::class,
+            BrandService::class
+        );
+        //Order
+        $this->app->singleton(
+            OrderRepositoryInterface::class,
+            OrderRepository::class
+        );
+        $this->app->singleton(
+            OrderServiceInterface::class,
+            OrderService::class
+        );
+        //OrderDetail
+        $this->app->singleton(
+            OrderDetailRepositoryInterface::class,
+            OrderDetailRepository::class
+        );
+        $this->app->singleton(
+            OrderDetailServiceInterface::class,
+            OrderDetailService::class
         );
     }
 
